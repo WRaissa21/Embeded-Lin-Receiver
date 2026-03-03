5832c5ca72059f236bdb6383fe3f89c749e3259d
WRaissa21 <witanodanwe@gmail.com>
2026-03-03 15:45:33 +0100

    Delete LINReceiver_lib/ps/InterfaceMicroprocesseur_LINArchitecture/hds directory

--- LINReceiver_lib/ps/InterfaceMicroprocesseur_LINArchitecture/hds/add_files.tcl
+++
@@ -1,3 +0,0 @@
-add_input_file {"C:/Users/E24A503D/Desktop/LINReceiver/LINReceiver/LINReceiver_lib/hdl/InterfaceMicroprocesseur_LINArchitecture.vhd"} -format {VHDL_2008} -work LINReceiver_lib
-setup_design -design InterfaceMicroprocesseur
-setup_design -architecture LINArchitecture
