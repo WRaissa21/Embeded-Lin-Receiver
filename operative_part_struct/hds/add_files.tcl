13d3793fa837927a9cfb41e55173f819bd6b3bb5
WRaissa21 <witanodanwe@gmail.com>
2026-03-03 15:42:24 +0100

    Delete operative_part_struct/hds directory

--- operative_part_struct/hds/add_files.tcl
+++
@@ -1,4 +0,0 @@
-add_input_file {"C:/Users/e229154f/Documents/HDS/operative_part/operative_part_lib/hdl/decoder_tbl.vhd"} -format {VHDL} -work operative_part_lib
-add_input_file {"C:/Users/e229154f/Documents/HDS/operative_part/operative_part_lib/hdl/operative_part_struct.vhd"} -format {VHDL} -work operative_part_lib
-setup_design -design operative_part
-setup_design -architecture struct
