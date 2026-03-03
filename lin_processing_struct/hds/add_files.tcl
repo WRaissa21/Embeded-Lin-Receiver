455b987a212e2c589671e9106cd37aa29104020a
WRaissa21 <witanodanwe@gmail.com>
2026-03-03 15:43:28 +0100

    Delete lin_processing_struct/hds directory

--- lin_processing_struct/hds/add_files.tcl
+++
@@ -1,6 +0,0 @@
-add_input_file {"C:/Users/e229154f/Documents/HDS/operative_part/operative_part_lib/hdl/command_unit_arch.vhd"} -format {VHDL_2008} -work operative_part_lib
-add_input_file {"C:/Users/e229154f/Documents/HDS/operative_part/operative_part_lib/hdl/decoder_tbl.vhd"} -format {VHDL} -work operative_part_lib
-add_input_file {"C:/Users/e229154f/Documents/HDS/operative_part/operative_part_lib/hdl/operative_part_struct.vhd"} -format {VHDL} -work operative_part_lib
-add_input_file {"C:/Users/e229154f/Documents/HDS/operative_part/operative_part_lib/hdl/lin_processing_struct.vhd"} -format {VHDL} -work operative_part_lib
-setup_design -design lin_processing
-setup_design -architecture struct
